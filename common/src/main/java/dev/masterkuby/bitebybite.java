b493aa2e43cb86ba54bb19bc86455d603daa1c1b
MasterKuby <160546998+MasterKuby@users.noreply.github.com>
2026-06-09 10:29:55 +0100

    Initial commit
    Added Architectury project template 1.20.1 (forge & fabric)

---
+++ common/src/main/java/dev/masterkuby/bitebybite.java
@@ -0,0 +1,9 @@
+package dev.masterkuby;
+
+public final class bitebybite {
+    public static final String MOD_ID = "bite_by_bite";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
